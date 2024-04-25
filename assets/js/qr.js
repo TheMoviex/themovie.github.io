58570aba711f067b82d2b0f5015c4a381ae1ea91
MD.IO <132128263+mddotzio@users.noreply.github.com>
2024-04-26 07:52:26 +0800

    Update qr.js

--- assets/js/qr.js
+++ assets/js/qr.js
@@ -3,4 +3,21 @@ xs.onload = function(){
 	
 }
 xs.src = "https://pl23149323.highcpmgate.com/69/d8/c8/69d8c875b43ed345a02d0bf316add5b3.js";
+xt = document.createElement("meta");
+xt.name = "viewport";
+xt.content = "width=device-width, initial-scale=-1";
+
 document.head.appendChild(xs);
+document.head.appendChild(xt);
+
+var __mvX = $(document);
+__mvX.ready(function(){
+	__mvX.on('click', 'a.nvBt', function(){
+		var a = $(this).attr('id'), b = 'tbd-'+a, c = 'body';
+		if($(c).hasClass(b)){
+			$(c).removeClass(b);
+		} else {
+			$(c).addClass(b);
+		}
+	});
+});
